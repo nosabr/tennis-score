66f6fc1b5463639c8627cf5764c20a5e4c0e4388
nosabr <syrym.sabyrzhan@gmail.com>
2025-09-08 12:46:12 +0500

    home page servlet done

--- src/main/java/com/example/tennisscoreboard2025/servlets/HomeServlet.java
+++ src/main/java/com/example/tennisscoreboard2025/servlets/HomeServlet.java
@@ -9,9 +9,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet ("/home")
+
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/views/home.jsp").forward(req,resp);
+        req.getRequestDispatcher("views/home.jsp").forward(req,resp);
     }
 }
--- src/main/webapp/views/home.jsp
+++ src/main/webapp/views/home.jsp
@@ -1,10 +1,4 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: user
-  Date: 08.09.2025
-  Time: 10:39
-  To change this template use File | Settings | File Templates.
---%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <!doctype html>
 <html lang="ru">
