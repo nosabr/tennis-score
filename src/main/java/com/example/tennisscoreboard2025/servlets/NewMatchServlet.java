0f7e43865cb6963b013c77ebb4087582dfc5f50c
nosabr <syrym.sabyrzhan@gmail.com>
2025-09-10 11:46:34 +0500

    new match servlet start

--- src/main/java/com/example/tennisscoreboard2025/servlets/NewMatchServlet.java
+++ src/main/java/com/example/tennisscoreboard2025/servlets/NewMatchServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class NewMatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/views/start-match.jsp").forward(req,resp);
+        req.getRequestDispatcher("/views/new-match.jsp").forward(req,resp);
     }
 
     @Override
