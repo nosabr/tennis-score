5aeb2f32f7d2726a9eb2fa8bf7f3ed825d4dfb83
nosabr <syrym.sabyrzhan@gmail.com>
2025-09-10 11:12:36 +0500

    hiber хз вроде настроил хрен знает чо сделал

--- src/main/java/com/example/tennisscoreboard2025/entity/Player.java
+++ src/main/java/com/example/tennisscoreboard2025/entity/Player.java
@@ -1,8 +1,16 @@
 package com.example.tennisscoreboard2025.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "players")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+
+    // getters/setters
 }
