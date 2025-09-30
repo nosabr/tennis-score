06f9e6c0c87eb0e5d3a4d45c51548d75709f263d
nosabr <syrym.sabyrzhan@gmail.com>
2025-09-30 18:47:36 +0500

    hiber/ threads

--- src/main/java/com/example/tennisscoreboard2025/util/HibernateSessionFactoryUtil.java
+++ src/main/java/com/example/tennisscoreboard2025/util/HibernateSessionFactoryUtil.java
@@ -1,7 +1,12 @@
 package com.example.tennisscoreboard2025.util;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 
 public class HibernateSessionFactoryUtil {
-    //SessionFactory sessionFactory;
+    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
+
+
 }
