6acc96290a36468169759de8caabf19b8d5e6be7
nosabr <syrym.sabyrzhan@gmail.com>
2025-10-01 11:05:49 +0500

    hiber/ threads

--- src/main/java/com/example/tennisscoreboard2025/util/HibernateSessionFactoryUtil.java
+++ src/main/java/com/example/tennisscoreboard2025/util/HibernateSessionFactoryUtil.java
@@ -1,12 +1,27 @@
 package com.example.tennisscoreboard2025.util;
+import com.example.tennisscoreboard2025.models.Match;
+import com.example.tennisscoreboard2025.models.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
 public class HibernateSessionFactoryUtil {
-    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    Session session = sessionFactory.getCurrentSession();
+    public static SessionFactory sessionFactory;
+    private HibernateSessionFactoryUtil(){};
 
-
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                sessionFactory = new Configuration().
+                        configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Player.class)
+                        .addAnnotatedClass(Match.class)
+                        .buildSessionFactory();
+            } catch (Exception e) {
+                System.out.println("Exception caught in HibernateSessionFactoryUtil" + e.getMessage());
+            }
+        }
+        return sessionFactory;
+    }
 }
