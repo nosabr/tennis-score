1cd27559ef87a4047bb8ac072317871326c48685
nosabr <syrym.sabyrzhan@gmail.com>
2025-10-03 12:25:26 +0500

    player dao save

--- src/main/java/com/example/tennisscoreboard2025/dao/PlayerDAO.java
+++ src/main/java/com/example/tennisscoreboard2025/dao/PlayerDAO.java
@@ -2,15 +2,16 @@ package com.example.tennisscoreboard2025.dao;
 
 import com.example.tennisscoreboard2025.models.Player;
 import com.example.tennisscoreboard2025.util.HibernateSessionFactoryUtil;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class PlayerDAO {
     public void save(Player p) {
-        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
-            Transaction tx = session.beginTransaction();
-            session.persist(p);
-            tx.commit();
-        }
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(p);
+        tx.commit();
+        session.close();
     }
 }
