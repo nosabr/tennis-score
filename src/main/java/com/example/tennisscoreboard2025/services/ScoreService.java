a6da73699dcd06f6c8e4aa871c6f23d800adb8ab
nosabr <syrym.sabyrzhan@gmail.com>
2025-10-21 10:32:05 +0500

    hzhzz

---
+++ src/main/java/com/example/tennisscoreboard2025/services/ScoreService.java
@@ -0,0 +1,10 @@
+package com.example.tennisscoreboard2025.services;
+
+import com.example.tennisscoreboard2025.models.Match;
+
+public class ScoreService {
+    public Match addPointToPlayer(Match match, int scoredPlayerNumber) {
+
+        return match;
+    }
+}
