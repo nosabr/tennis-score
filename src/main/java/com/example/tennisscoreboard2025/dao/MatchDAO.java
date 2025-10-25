b23dde006ea5247670d2d810d716d5bdd3f7f404
nosabr <syrym.sabyrzhan@gmail.com>
2025-10-25 19:08:08 +0500

    Добавил ДАО для матчей

--- src/main/java/com/example/tennisscoreboard2025/dao/MatchDAO.java
+++ src/main/java/com/example/tennisscoreboard2025/dao/MatchDAO.java
@@ -4,6 +4,7 @@ import com.example.tennisscoreboard2025.models.Match;
 import com.example.tennisscoreboard2025.util.HibernateSessionFactoryUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,27 @@ public class MatchDAO {
         return Optional.ofNullable(match);
     }
 
+    public int countByName(String name) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query<Long> query = session.createQuery("SELECT COUNT(m) FROM Match m " +
+                "WHERE m.player1.name LIKE :name OR m.player2.name LIKE :name");
+        query.setParameter("name","%" + name + "%");
+        session.close();
+        return query.uniqueResult().intValue();
+    }
+
+    public List<Match> findByName(String name, int page, int pageSize){
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query<Match> query = session.createQuery("FROM Match m WHERE m.player1.name " +
+                "LIKE :name OR m.player2.name LIKE :name " +
+                "ORDER BY m.id ASC", Match.class);
+        query.setParameter("name","%" + name + "%");
+        query.setFirstResult((page-1)*pageSize);
+        query.setMaxResults(pageSize);
+        session.close();
+        return query.getResultList();
+    }
+
     public Optional<Match> findByName(String name) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         String hql = "FROM Match m WHERE m.player1.name = :name or m.player2.name = :name";
@@ -25,6 +47,21 @@ public class MatchDAO {
         return matches.stream().findFirst();
     }
 
+    public int count(){
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query<Long> query = session.createQuery("select count(m) from Match m", Long.class);
+        session.close();
+        return query.uniqueResult().intValue();
+    }
+
+    public List<Match> findAll(int page, int pageSize){
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Query<Match> query = session.createQuery("FROM Match m ORDER BY m.id DESC", Match.class);
+        query.setFirstResult((page - 1) * pageSize);
+        query.setMaxResults(pageSize);
+        session.close();
+        return query.list();
+    }
     public List<Match> findAll() {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         List<Match> matchList = session.createQuery("FROM Match", Match.class).list();
